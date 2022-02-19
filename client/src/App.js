0361cd5d48ea708334a7506443e1b38ff21e3112
henry <henry@iMac-home.local>
2022-02-19 21:45:21 +0900

    채팅화면 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,71 @@
-import styled from "styled-components"
-import io from "socket.io-client"
-import Chat from "./Chat"
-import {useState} from "react"
+import styled from "styled-components";
+import io from "socket.io-client";
+import Chat from "./Chat";
+import { useState } from "react";
 
-const socket = io.connect("http://localhost:3001")
+const socket = io.connect("http://localhost:3001");
 
 const Container = styled.div`
-    width: 100vw;
-    heigit: 100vh;
-    margin: 0 auto;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items:center;
-    input {
-      width:90%;
-    }
-    button {
-      
-    }
-    @media (min-width: 500px){
-      width: 600px;
-    }
+  width: 100vw;
+  heigit: 100vh;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  font-family: "Montserrat", sans-serif;
+  input {
+    width: 90%;
+  }
+  button {
+  }
+  @media (min-width: 500px) {
+    width: 600px;
+  }
 `;
 
 function App() {
   const [username, setUsername] = useState("");
   const [room, setRoom] = useState("");
-  const [showChat, setShowChat] = useState(false)
+  const [showChat, setShowChat] = useState(false);
 
-  const inputUserName=(event)=>{
-    setUsername(event.target.value)
-  }
-  const inputRoomName = (event)=>{
-    setRoom(event.target.value)
-  }
+  const inputUserName = (event) => {
+    setUsername(event.target.value);
+  };
+  const inputRoomName = (event) => {
+    setRoom(event.target.value);
+  };
 
   const joinRoom = () => {
     if (username !== "" && room !== "") {
       socket.emit("join_room", room);
-      setShowChat(true)
+      setShowChat(true);
     }
   };
-  
 
   return (
-    <Container >
-    {!showChat ? ( 
-      <>
-        <h3>Join a Chat</h3>
-        <input type="text" placeholder="what .... ?" onChange={inputUserName}></input>
-        <input 
-        type="text" 
-        placeholder="Room ID .... ?" 
-        onChange={inputRoomName}
-        onKeyPress={(event) =>{event.key === "Enter" && joinRoom()}}
-        ></input>
-        <button onClick={joinRoom}>Join A Room</button>
-      </>
-      ):(
+    <Container>
+      {!showChat ? (
+        <>
+          <h3>Join a Chat</h3>
+          <input
+            type="text"
+            placeholder="what .... ?"
+            onChange={inputUserName}
+          ></input>
+          <input
+            type="text"
+            placeholder="Room ID .... ?"
+            onChange={inputRoomName}
+            onKeyPress={(event) => {
+              event.key === "Enter" && joinRoom();
+            }}
+          ></input>
+          <button onClick={joinRoom}>Join A Room</button>
+        </>
+      ) : (
         <Chat socket={socket} username={username} room={room} />
-      )
-    }
-     
-      
+      )}
     </Container>
   );
 }
--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,107 +1,159 @@
-import React,{useState, useEffect} from 'react';
-import styled from "styled-components"
-import ScrollToBottom from "react-scroll-to-bottom"
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import ScrollToBottom from "react-scroll-to-bottom";
 
 const Wrapper = styled.div`
-    width: 100%;
-    display: flex;
-    flex-Direction: column;
-    justify-content: space-between;
-    align-items: center;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  border: 4px solid maroon;
 `;
 
 const ChatArea = styled.div`
-    width:90%;
-    height: 80vh;
-    background-color: mistyRose;
-    display: flex;
-    // flex-Direction: column-reverse;
-    overflow : auto;
+  width: 90%;
+  height: 80vh;
+  background-color: mistyRose;
+  display: flex;
+  flex-direction: column-reverse;
+  overflow: auto;
+  border: 4px solid chocolate;
 `;
 
- const MessageBox = styled.div`
-    padding: 10px;
-    display:flex;
-    flex-Direction: row;
-    justify-Content: space-between;
-    font-size: 13px;
- `;
+const MessageContainer = styled.div`
+  width: 90%;
+  margin: 10px;
+  padding: 0 10px;
+  display: flex;
+  ${(props) =>
+    props.whoIsAuthor === "me"
+      ? `justify-content: flex-end;`
+      : `justify-content: flex-start;`}
+`;
 
- const MessageText = styled.div`
-    width: 90%;
-    padding: 10px;
+const MessageBox = styled.div`
+  padding: 0 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  font-size: 13px;
+  ${(props) =>
+    props.whoIsAuthor === "me"
+      ? `align-items: flex-end;`
+      : `align-items: flex-start;`}
+`;
+
+const MessageText = styled.div`
+  width: 90%;
+  padding: 10px;
+  border-radius: 10px;
+
+  color: white;
+  font-size: 13px;
+  ${(props) =>
+    props.whoIsAuthor === "me"
+      ? `background-color: darkkhaki;`
+      : `background-color: steelblue;`}
+`;
+
+const SubTextBox = styled.div`
+  font-size: 10px;
+  padding: 0 5px;
+`;
+
+const InputArea = styled.div`
+  width: 100%;
+  padding: 5px 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  input {
+    padding: 5px;
+    margin: 2px 2px;
     border-radius: 10px;
-    background-color: chocolate; 
-    font-size: 13px;
- `;
+    border: 0px solid black;
+    background-color: peru;
+  }
+`;
 
- const InputArea = styled.div`
-    width: 90%;
-    display: flex;
-    flex-Direction: row;
- `;
+function Chat({ socket, username, room }) {
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
 
+  const changeMessage = (event) => {
+    setCurrentMessage(event.target.value);
+  };
 
-
-function Chat({socket, username, room}) {
-    const [currentMessage, setCurrentMessage] = useState("");
-    const [messageList, setMessageList] = useState([]);
-
-    const changeMessage=(event)=>{
-        setCurrentMessage(event.target.value)
+  const sendMessage = async (event) => {
+    if (currentMessage !== "") {
+      const messageData = {
+        room: room,
+        author: username,
+        message: currentMessage,
+        time:
+          String(new Date(Date.now()).getHours()).padStart(2, "0") +
+          ":" +
+          String(new Date(Date.now()).getMinutes()).padStart(2, "0"),
+      };
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
     }
-    const sendMessage= async (event)=>{
-        if(currentMessage !== ""){
-            const messageData={
-                room:room,
-                author: username,
-                message: currentMessage,
-                time: String(new Date(Date.now()).getHours()).padStart(2,"0") 
-                + ":" + String(new Date(Date.now()).getMinutes()).padStart(2,"0")
-            }
-            await socket.emit("send_message", messageData)
-            setMessageList(list=>[...list, messageData])
-            setCurrentMessage("")
-            
-        }
-    }
+  };
 
-    useEffect(()=>{
-        socket.on("receive_message", (data)=>{
-            setMessageList(list =>[...list, data])
-        })
-    },[socket])
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageList((list) => [...list, data]);
+    });
+  }, [socket]);
 
-    return (
-        <Wrapper>
-            <div>
-                <p>LIVE CHAT</p>
-                <p>{room}</p>
-            </div>
-            <ChatArea>
-            <ScrollToBottom>
-             {messageList.map((data, index) => 
-             <MessageBox key={index} whoIsAuthor={username === data.author ? "me" : "other"}>
-                <div>
-                    <div>{data.author === username ? "me": data.author } </div>
-                    <MessageText>{data.message}</MessageText>
-                </div>
-                <div> {data.time}</div>
-             </MessageBox>
-            )}   
-            </ScrollToBottom>     
-            </ChatArea>
-            <InputArea>
-                <input 
-                type="text" 
-                placeholder="....." 
-                onChange={changeMessage} 
-                value={currentMessage} 
-                onKeyPress={(event) =>{event.key === "Enter" && sendMessage()}} />
-                <button onClick={sendMessage}>&#9658;</button>
-            </InputArea> 
-        </Wrapper>
-    );
+  return (
+    <Wrapper>
+      <div>
+        <p>LIVE CHAT</p>
+        <p>ROOM : {room}</p>
+      </div>
+
+      <ChatArea>
+        <ScrollToBottom>
+          {messageList.map((data, index) => (
+            <MessageContainer
+              whoIsAuthor={username === data.author ? "me" : "other"}
+            >
+              <MessageBox
+                key={index}
+                whoIsAuthor={username === data.author ? "me" : "other"}
+              >
+                <SubTextBox>
+                  {data.author === username ? "me" : data.author}
+                </SubTextBox>
+                <MessageText
+                  whoIsAuthor={username === data.author ? "me" : "other"}
+                >
+                  {data.message}
+                </MessageText>
+                <SubTextBox> {data.time}</SubTextBox>
+              </MessageBox>
+            </MessageContainer>
+          ))}
+        </ScrollToBottom>
+      </ChatArea>
+
+      <InputArea>
+        <input
+          type="text"
+          placeholder="....."
+          onChange={changeMessage}
+          value={currentMessage}
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
+          }}
+        />
+        {/* <div onClick={sendMessage}>&#9658;</div> */}
+      </InputArea>
+    </Wrapper>
+  );
 }
 
 export default Chat;
