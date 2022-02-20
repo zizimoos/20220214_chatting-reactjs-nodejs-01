1011c05f34594ba4f60407317177e4e6f44a438b
henry <henry@iMac-home.local>
2022-02-20 18:18:07 +0900

    ......what

--- server/index.js
+++ server/index.js
@@ -1,36 +1,36 @@
 const express = require("express");
-const http = require("http")
-const cors = require("cors")
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
-app.use(cors())
-const server = http.createServer(app)
+app.use(cors());
+const server = http.createServer(app);
 
-const { Server } = require("socket.io");
 const io = new Server(server, {
-    cors:{
-        origin :"http://localhost:3000",
-        methods:[ "GET" , "POST" ]
-    }
-})
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
-io.on("connection",(socket)=>{
-    console.log(`USER CONNECTED : ${socket.id}`)
+io.on("connection", (socket) => {
+  console.log(`USER CONNECTED : ${socket.id}`);
 
-    socket.on("join_room", (data) => {
-        socket.join(data);
-        console.log(`User with ID: ${socket.id} joined room: ${data}`);
-    });
-    socket.on("send_message",(data)=>{
-        console.log(data)
-        socket.to(data.room).emit("receive_message", data)
-    })
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+  socket.on("send_message", (data) => {
+    console.log(data);
+    socket.to(data.room).emit("receive_message", data);
+  });
 
-    socket.on("disconnect",()=>{
-        console.log("User Disconnected",socket.id)
-    })
-})
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
-server.listen(3001, ()=>{
-    console.log("SERVER RUNNING")
-})
+server.listen(3001, () => {
+  console.log("SERVER RUNNING");
+});
